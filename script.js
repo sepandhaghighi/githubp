3dd9105a4cbd027e9093597d716a06931b4ea87f
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-06-01 18:24:22 +0330

    feat : add Modal object

--- script.js
+++ script.js
@@ -22,43 +22,45 @@ const CONFIG = {
   }
 }
 
-function showAlert(options) {
-  return Swal.fire({
-    buttonsStyling: false,
-    customClass: {
-      container: "githubp-swal-container",
-      popup: "githubp-swal",
-      icon: "githubp-swal-icon",
-      title: "githubp-swal-title",
-      htmlContainer: "githubp-swal-text",
-      actions: "githubp-swal-actions",
-      confirmButton: "githubp-swal-button githubp-swal-button-confirm",
-      cancelButton: "githubp-swal-button githubp-swal-button-cancel"
-    },
-    ...options
-  });
-}
+const Modal = {
+  fire(options) {
+    return Swal.fire({
+      buttonsStyling: false,
+      customClass: {
+        container: "githubp-swal-container",
+        popup: "githubp-swal",
+        icon: "githubp-swal-icon",
+        title: "githubp-swal-title",
+        htmlContainer: "githubp-swal-text",
+        actions: "githubp-swal-actions",
+        confirmButton: "githubp-swal-button githubp-swal-button-confirm",
+        cancelButton: "githubp-swal-button githubp-swal-button-cancel"
+      },
+      ...options
+    });
+  },
 
-function showError(message) {
-  return showAlert({
-    icon: "error",
-    iconColor: "#d73a49",
-    title: "Error",
-    text: message,
-    confirmButtonText: "OK"
-  });
-}
+  error(message) {
+    return this.fire({
+      icon: "error",
+      iconColor: "#d73a49",
+      title: "Error",
+      text: message,
+      confirmButtonText: "OK"
+    });
+  },
 
-function showConfirm(message) {
-  return showAlert({
-    icon: "question",
-    iconColor: "#62B039",
-    title: "Confirm",
-    text: message,
-    showCancelButton: true,
-    confirmButtonText: "Yes",
-    cancelButtonText: "Cancel"
-  });
+  confirm(message) {
+    return this.fire({
+      icon: "question",
+      iconColor: "#62B039",
+      title: "Confirm",
+      text: message,
+      showCancelButton: true,
+      confirmButtonText: "Yes",
+      cancelButtonText: "Cancel"
+    });
+  },
 }
 
 function getRecent() {
